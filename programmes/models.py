23e8932e9373d9ed09c071a4e25270a0ec8dc16f
RoxJade <roxannejade88@icloud.com>
2022-03-24 21:58:09 +0000

    Add programmes and categories models, migrated

--- programmes/models.py
+++ programmes/models.py
@@ -2,31 +2,35 @@ from django.db import models
 
 
 class Category(models.Model):
+    '''return category model'''
 
     class Meta:
+        '''return category as plural'''
         verbose_name_plural = 'Categories'
 
     name = models.CharField(max_length=250)
     friendly_name = models.CharField(max_length=250, null=True, blank=True)
 
     def __str__(self):
-        return self.name
+        '''return category name'''
+        return str(self.name)
 
     def get_friendly_name(self):
+        '''return friendly category name'''
         return self.friendly_name
 
 
-class Product(models.Model):
+class Programmes(models.Model):
+    '''return product model'''
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
-    programme_id=
     name = models.CharField(max_length=250)
     description = models.TextField()
     programme_length = models.CharField(max_length=250)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
-    programme_cost(models.DecimalField())
+    programme_cost = models.DecimalField(max_digits=6, decimal_places=2)
 
     def __str__(self):
-        return self.name
-
+        '''return product model'''
+        return str(self.name)
