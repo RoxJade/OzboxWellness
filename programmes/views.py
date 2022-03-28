90e7f5a36cfc0260ef26901e7ac9a17465e66f2a
RoxJade <roxannejade88@icloud.com>
2022-03-28 11:35:22 +0000

    Add programmes view

--- programmes/views.py
+++ programmes/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Programmes
 
 # Create your views here.
+
+
+def all_programmes(request):
+    """A view to show all programmes"""
+
+    programmes = Programmes.objects.all()
+
+    context = {
+        'programmes': programmes,
+    }
+
+    return render(request, 'programmes/programmes.html', context)
