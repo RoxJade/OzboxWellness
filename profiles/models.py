8f2031bd46d23ed980ec8755de38a97507ed8ba7
RoxJade <roxannejade88@icloud.com>
2022-07-06 11:58:02 +0000

    add profile models

--- profiles/models.py
+++ profiles/models.py
@@ -3,8 +3,36 @@ from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
 
+
 class UserProfile(models.Model):
     """
     A user profile model for maintaining default
     delivery information, customer info and order history
     """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    default_phone_number = models.CharField(
+        max_length=11, null=True, blank=True)
+    default_street_address = models.CharField(
+        max_length=80, null=True, blank=True)
+    default_town_or_city = models.CharField(
+        max_length=40, null=True, blank=True)
+    default_postcode = models.CharField(max_length=20, null=True, blank=True)
+    default_county = models.CharField(max_length=80, null=True, blank=True)
+    your_health_goals = models.TextField(null=True, blank=True)
+    illness_or_conditions = models.TextField(null=True, blank=False)
+    nok_name = models.CharField(max_length=40, null=True, blank=False)
+    nok_contact = models.CharField(max_length=30, null=True, blank=False)
+
+    def __str__(self):
+        return self.user.username
+
+
+@receiver(post_save, sender=User)
+def create_or_update_user_profile(sender, instance, created, **kwargs):
+    """
+    Create or update the user profile
+    """
+    if created:
+        UserProfile.objects.create(user=instance)
+    # Existing users: just save the profile
+    instance.userprofile.save()
